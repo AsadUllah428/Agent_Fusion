4f509ac33a7c53b336946fbed9cd2c2defdab8c9
Asad Munir <asadmunir428@gmail.com>
2025-10-01 13:12:28 +0500

    Add files via upload

---
+++ 01. Simple_Langchain_Agent.py
@@ -0,0 +1,53 @@
+# !pip install langgraph langsmith
+# !pip install langchain langchain_groq langchain_community
+# !pip install langchain-groq
+from langchain_groq import ChatGroq
+import os
+from typing import Annotated
+from typing_extensions import TypedDict
+from langgraph.graph import StateGraph,START,END
+from langgraph.graph.message import add_messages
+
+
+groq_api_key="Your Groq API Key"
+langsmith="Your Langsmith Key"
+
+os.environ["LANGCHAIN_API_KEY"] = langsmith
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+
+llm = ChatGroq(
+    groq_api_key="Your Groq API Key",
+    model_name="Your model name"  # or another available Groq model
+)
+
+class State(TypedDict):
+  # Messages have the type "list". The `add_messages` function
+    # in the annotation defines how this state key should be updated
+    # (in this case, it appends messages to the list, rather than overwriting them)
+  messages:Annotated[list,add_messages]
+
+graph_builder=StateGraph(State)
+graph_builder
+
+def chatbot(state:State):
+  return {"messages":llm.invoke(state['messages'])}
+
+graph_builder.add_node("chatbot",chatbot)
+
+# graph_builder
+
+graph_builder.add_edge(START,"chatbot")
+graph_builder.add_edge("chatbot",END)
+
+graph=graph_builder.compile()
+
+while True:
+  user_input=input("User: ")
+  if user_input.lower() in ["quit","q"]:
+    print("Good Bye")
+    break
+  for event in graph.stream({'messages':("user",user_input)}):
+    print(event.values())
+    for value in event.values():
+      print(value['messages'])
+      print("Assistant:",value["messages"].content)
